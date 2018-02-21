70f3d9120497fb481883ffe83047adafdac6cc6d
Simon Weller <s.meierbeck@gmail.com>
2018-02-21 22:37:52 +0100

    full coverage for (and some refactoring of) DataTableRow

--- client/src/components/DataTable/DataTableRow/DataTableRow.js
+++ client/src/components/DataTable/DataTableRow/DataTableRow.js
@@ -5,19 +5,33 @@ import classes from './DataTableRow.css';
 
 const DataTableRow = (props) => {
 
+    const rowData = props.rowData ?
+        props.rowData.map((column, i) => <td key={i}>{column}</td>)
+        : null;
+    const editLink = props.match && props.id !== undefined ? 
+        <Link to={props.match.url + '/' + props.id}>
+            Edit
+        </Link>
+        : null;
+    const deleteButton = props.deleteHandler ?
+        <button onClick={() => props.deleteHandler(props.id)}>
+            Delete
+        </button>
+        : null;
+    const actions = editLink || deleteButton ? 
+        <td className={classes.actions}>
+            {editLink}
+            {deleteButton}
+        </td>
+        : null;
+    
     return (
         <tr>
-            {props.rowData.map((column, i) => <td key={i}>{column}</td>)}
-            <td className={classes.actions}>
-                <Link to={props.match.url + '/' + props.id}>
-                    Edit
-                </Link>
-                <button onClick={() => props.deleteHandler(props.id)}>
-                    Delete
-                </button>
-            </td>
+            {rowData}
+            {actions}
         </tr>
     );
 };
 
 export default withRouter(DataTableRow);
+export { DataTableRow };
---
+++ client/src/components/DataTable/DataTableRow/DataTableRow.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { DataTableRow } from './DataTableRow';
+
+describe('DataTableRow', () => {
+    let shallowDataTableRow;
+    let props;
+    const dataTableRow = (props) => {
+        if (!shallowDataTableRow) {
+            shallowDataTableRow = shallow(
+                <DataTableRow {...props} />
+            );
+            return shallowDataTableRow;
+        }
+    }
+
+    beforeEach(() => {
+        shallowDataTableRow = undefined;
+        props = {
+            rowData: undefined,
+            deleteHandler: undefined,
+            match: undefined,
+            id: undefined,
+        }
+    });
+
+    it('renders correctly', () => {
+        const shallowRow = dataTableRow(props);
+        expect(shallowRow).toMatchSnapshot();
+    });
+
+    it('renders a td per rowData element passed in', () => {
+        props.rowData = ['Simon', 'Weller'];
+        const shallowRow = dataTableRow(props);
+        expect(shallowRow.find('td').length).toBe(2);
+    });
+
+    it('renders a delete button in the action cell if a deleteHandler is passed in', () => {
+        props.deleteHandler = () => {};
+        const shallowRow = dataTableRow(props);
+        expect(shallowRow.find('.actions button').length).toBe(1);
+    });
+
+    it('calls the deleteHandler with the id when the delete button is clicked', () => {
+        const deleteHandler = jest.fn();
+        props.deleteHandler = deleteHandler;
+        props.id = 27;
+        const shallowRow = dataTableRow(props);
+        shallowRow.find('.actions button').simulate('click');
+        expect(deleteHandler).toBeCalledWith(27);
+    });
+
+    it('renders an edit link in the action cell if an id is passed in and a url can be matched', () => {
+        props.id = 0;
+        props.match = {
+            url: ''
+        }
+        const shallowRow = dataTableRow(props);
+        expect(shallowRow.find('.actions Link').length).toBe(1);
+    });
+
+});
