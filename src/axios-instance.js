6bbee4162d742858770b16276cad879f82031d08
Simon Weller <simon.weller@inducon.ag>
2018-06-24 20:24:35 +0200

    splitting server code into separate repo

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let apiPath = 'https://sw-address-book-server.herokuapp.com/api/';
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    apiPath = '/api/';
+    apiPath = 'http://localhost:3001/api/';
 }
 
 const instance = axios.create({
