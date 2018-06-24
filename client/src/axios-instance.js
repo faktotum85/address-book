e54291003ece6a5a8023df760013c6241631ca0e
Simon Weller <simon.weller@inducon.ag>
2018-06-24 15:35:43 +0200

    changing api location depending on the environment

--- client/src/axios-instance.js
+++ client/src/axios-instance.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+let apiPath = 'https://sw-address-book-server.herokuapp.com/api/';
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    apiPath = '/api/';
+}
+
 const instance = axios.create({
-    baseURL: '/api/',
+    baseURL: apiPath
 });
 
 export default instance;
