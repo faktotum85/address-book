8e6a949c50cde20b3a1d4f839009c252b2ab52cf
Simon Weller <s.meierbeck@gmail.com>
2018-06-24 23:36:02 +0200

    conditionally showing different auth links

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, Switch, Redirect, NavLink, withRouter } from 'react-router-dom';
 import Wrapper from '../hoc/Wrapper/Wrapper';
 
 import Persons from './Persons/Persons';
 import Auth from './Auth/Auth';
 import classes from './App.css';
 
-class App extends Component {
+export class App extends Component {
     render() {
+        const authButtons = this.props.username 
+            ? <li><NavLink to="/logout" activeClassName={classes.active}>Log Out</NavLink></li>
+            : <React.Fragment>
+                <li><NavLink to="/signup" activeClassName={classes.active}>Sign up</NavLink></li>
+                <li><NavLink to="/login" activeClassName={classes.active}>Log in</NavLink></li>
+              </React.Fragment>;
+
         return (
             <Wrapper>
                 <nav className={classes.MainNav}>
                     <ul>
                         <li><NavLink to="/persons" exact activeClassName={classes.active}>List</NavLink></li>
                         <li><NavLink to="/persons/new" activeClassName={classes.active}>Add Person</NavLink></li>
-                        <li><NavLink to="/signup" activeClassName={classes.active}>Sign up</NavLink></li>
-                        <li><NavLink to="/login" activeClassName={classes.active}>Log in</NavLink></li>
+                        {authButtons}
                     </ul>
                 </nav>
                 <main>
@@ -31,4 +38,12 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        username: state.auth.username,
+        history: state
+    }
+};
+
+export const ConnectedApp = connect(mapStateToProps, null)(App);
+export default withRouter(connect(mapStateToProps, null)(App));
--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { shallow } from 'enzyme';
 
-import App from './App';
+import {ConnectedApp, App} from './App';
 
 describe('App', () => {
-    let shallowApp;
+    let shallowApp, shallowConnectedApp;
+    let store, initialState;
+    const mockStore = configureStore([thunk]);
+
     const app = () => {
         if (!shallowApp) {
             shallowApp = shallow(
@@ -14,16 +19,54 @@ describe('App', () => {
         return shallowApp;
     }
 
+    const makeConnectedApp = (initialState) => {
+        if (!shallowConnectedApp) {
+            store = mockStore(initialState);
+            shallowConnectedApp = shallow(
+                <ConnectedApp store={store}/>
+            ).shallow();
+        }
+        return shallowConnectedApp;
+    }
+
     beforeEach(() => {
         shallowApp = undefined;
+        shallowConnectedApp = undefined;
+        initialState = {
+            auth: {
+                username: 'Simon Weller',
+            }
+        }
     });
 
     it('always renders a nav', () => {
         const navs = app().find('nav');
         expect(navs.length).toBe(1);
     });
+
     it('always renders a main', () => {
         const main = app().find('main');
         expect(main.length).toBe(1);
+    });
+
+    it('renders a logout link if a user is logged in', () =>  {
+        const app = makeConnectedApp({
+            auth: {
+                username: 'Simon Weller',
+            }
+        });
+        expect(app.find('NavLink[to="/logout"]').length).toBe(1);
+        expect(app.find('NavLink[to="/login"]').length).toBe(0);
+        expect(app.find('NavLink[to="/signup"]').length).toBe(0);    });
+
+    it(`renders a login and signup link if no user is logged in`, () =>  {
+        const app = makeConnectedApp({
+            auth: {
+                username: null,
+            }
+        });
+        expect(app.find('NavLink[to="/logout"]').length).toBe(0);
+        expect(app.find('NavLink[to="/login"]').length).toBe(1);
+        expect(app.find('NavLink[to="/signup"]').length).toBe(1);
     });
 });
