3d7414e9eeee420171605052d76b45355459b16f
Simon Weller <simon.weller@inducon.ag>
2018-06-24 21:15:20 +0200

    setting api url with env variable

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-let apiPath = 'https://sw-address-book-server.herokuapp.com/api/';
-if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    apiPath = 'http://localhost:3001/api/';
-}
-
 const instance = axios.create({
-    baseURL: apiPath
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default instance;
